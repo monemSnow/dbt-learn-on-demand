cf228c0216a9c0942466aa20f6937525e611b514
monem.metwally@qrious.co.nz <monem.metwally@qrious.co.nz>
2021-07-04 20:34:52 +0000

    Pivoted

--- models/marts/core/int_orders__pivoted.sql
+++ models/marts/core/int_orders__pivoted.sql
@@ -1,7 +1,16 @@
-{% set payment_method = ['bank_transfer','coupon','credit_card','gift_card'] %}
+{% set payment_methods = ['bank_transfer','coupon','credit_card','gift_card'] -%}
 
 with payments as (
     select *
     from {{ref('stg_payments')}}
 )
-select * from payments 
+select 
+    Order_id,
+    {% for payment_method in payment_methods -%}
+    sum (case when payment_method='{{payment_method}}' then amount else 0 end ) as {{payment_method}}_amount
+    {% if not loop.last -%}
+        ,
+    {% endif -%}
+    {% endfor -%}
+    from payments 
+    group by 1
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,7 +1,7 @@
 Select 
     id as payment_id,
     orderid as order_id,
-    paymentmethod,
+    paymentmethod as payment_method,
     status,
     -- Convert cents to dollar amount
     amount/100 as amount,
